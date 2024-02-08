a7a4d9ade4bb9011ddd5289f659921874c32c036
Mohammad Jafari <m.jafari9877@gmail.com>
2024-02-08 14:54:18 +0330

    calculate active-cookies with multiprocess

---
+++ src/quantcast_cli/filters.py
@@ -0,0 +1,34 @@
+from concurrent.futures import ProcessPoolExecutor
+from functools import reduce, partial
+
+import pandas as pd
+
+
+def filter_by_date(date: str, df: pd.DataFrame) -> pd.DataFrame:
+    return df[df.timestamp.str.startswith(date)]
+
+
+def calc_session_counts(date: str, df: pd.DataFrame) -> pd.Series:
+    return filter_by_date(date, df).groupby("cookie").size()
+
+
+def map_reduce(func, iterable):
+    with ProcessPoolExecutor() as executor:
+        results = [executor.submit(func, chunk) for chunk in iterable]
+
+    if not results:
+        return None
+
+    retrieved_results = map(lambda x: x.result(), results)
+    reduced_result = reduce(lambda acc, prev: acc + prev, retrieved_results)
+    return reduced_result
+
+
+def find_most_active_cookies(date: str, file, chunk_size: int = 10**5) -> list[str]:
+    chunks = pd.read_csv(file, chunksize=chunk_size)
+    counts = map_reduce(partial(calc_session_counts, date), chunks)
+
+    max_count: int = counts.max()
+    most_active_cookies_series: pd.Series = counts.loc[counts == max_count]
+    most_active_cookies: list[str] = most_active_cookies_series.index.tolist()
+    return most_active_cookies
