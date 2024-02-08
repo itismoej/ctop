608186612b872f47b44ca9f7167df8f9c247f365
Mohammad Jafari <m.jafari9877@gmail.com>
2024-02-09 00:25:46 +0330

    fix: non-present keys bug in sum of chunks

--- src/quantcast_cli/filters.py
+++ src/quantcast_cli/filters.py
@@ -15,19 +15,18 @@ def calc_session_counts(date: str, df: pd.DataFrame) -> pd.Series:
 def map_reduce(func, iterable):
     with ProcessPoolExecutor() as executor:
         results = [executor.submit(func, chunk) for chunk in iterable]
-
     if not results:
         return None
-
     retrieved_results = map(lambda x: x.result(), results)
-    reduced_result = reduce(lambda acc, prev: acc + prev, retrieved_results)
+    reduced_result = reduce(
+        lambda acc, prev: acc.add(prev, fill_value=0), retrieved_results
+    )
     return reduced_result
 
 
-def find_most_active_cookies(date: str, file, chunk_size: int = 10**5) -> list[str]:
+def find_most_active_cookies(date: str, file, chunk_size: int = 10**6) -> list[str]:
     chunks = pd.read_csv(file, chunksize=chunk_size)
     counts = map_reduce(partial(calc_session_counts, date), chunks)
-
     max_count: int = counts.max()
     most_active_cookies_series: pd.Series = counts.loc[counts == max_count]
     most_active_cookies: list[str] = most_active_cookies_series.index.tolist()
