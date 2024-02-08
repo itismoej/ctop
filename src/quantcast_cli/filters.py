8a0c2137d3b8ae99e584a60d23af9d62438e6d6c
Mohammad Jafari <m.jafari9877@gmail.com>
2024-02-09 01:11:32 +0330

    test: filter df by date

--- src/quantcast_cli/filters.py
+++ src/quantcast_cli/filters.py
@@ -1,4 +1,5 @@
 from concurrent.futures import ProcessPoolExecutor
+from datetime import datetime
 from functools import reduce, partial
 
 import pandas as pd
@@ -7,6 +8,13 @@ from .search import find_first_occurrence, find_last_occurrence, notfound
 
 
 def filter_by_date(date: str, df: pd.DataFrame) -> pd.DataFrame:
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError(
+            f"The date '{date}' does not match the ISO 8601 format 'YYYY-MM-DD'."
+        )
+
     first = find_first_occurrence(date, df)
     last = find_last_occurrence(date, df)
     if first is notfound or last is notfound:
